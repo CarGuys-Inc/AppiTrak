61d4491d2c41820d7ebbd06a580c578a9924eced
CarGuys Developer <developer@carguysinc.com>
2025-11-04 16:14:29 -0500

    feat: better user accounts

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,52 +3,22 @@ import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 
 export default async function DashboardRedirect() {
-  const supabase = await createClient();
-  const { data: { user } } = await supabase.auth.getUser();
+const supabase = await createClient();
+const { data: { user } } = await supabase.auth.getUser();
+if (!user) redirect("/login");
 
-  if (!user) redirect("/login");
 
-  console.log("User:", user);
-
-const { data: accountUser } = console.log(await supabase
-  .from("account_user")
-  .select(`
-    account_id,
-    account_role,
-    team_id,
-    accounts(id, slug, account_type, name),
-    teams(id, name, slug)
-  `)
+const { data: companyUser, error } = await supabase
+  .schema('basejump')
+  .from("company_users")
+  .select("company_id, role, companies(name, slug, account_type)")
   .eq("user_id", user.id)
-  .maybeSingle()
-)
-
-console.log("Account User:", accountUser);
-if (!personalAccount) redirect("/setup");
-
-// Step 2: determine the team account
-let teamAccountId = personalAccount.accounts.parent_account_id || personalAccount.account_id;
-
-const { data: teamAccount } = await supabase
-  .from("basejump.accounts")
-  .select("*")
-  .eq("id", teamAccountId)
   .maybeSingle();
+if (error) console.error(error)
 
-if (!teamAccount) redirect("/setup");
+  console.log(companyUser)
 
-// Step 3: optionally, fetch team_members or roles for RBAC
-const { data: teamMember } = await supabase
-  .from("basejump.team_members")
-  .select(`
-    team_id,
-    role,
-    teams(name, slug)
-  `)
-  .eq("user_id", user.id)
-  .eq("teams.account_id", teamAccount.id)
-  .maybeSingle();
+if (!companyUser) redirect("/setup");
 
-  redirect(`/dashboard/${teamAccount.slug}`);
-
+redirect(`/dashboard/${companyUser.companies.slug}`);
 }
